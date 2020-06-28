5e6ae83661583de14367ed9923cf6a6dfc87c6e5
Andrey Globa <Globa.Andrey@gmail.com>
2020-06-28 22:41:50 +0300

    to progress

---
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -0,0 +1,36 @@
+package ru.netology;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+
+public class CashbackHackServiceTest {
+
+    CashbackHackService test = new CashbackHackService();
+
+    @Test
+    public void shouldTestMoreRemain() {
+        // Сумма покупки менее 999$, требуется докупить
+        int actual = 1;
+        int expected = test.remain(999);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldTestRightRemain() {
+        // Сумма покупки 1000$, докупать не требуется
+        int expected = test.remain(1000);
+        int actual = 1000;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldTestMoreAndMoreRemain() {
+        // Сумма покупки 1001$, требуется докупить
+        int expected = test.remain(1001);
+        int actual = 999;
+        assertEquals(actual, expected);
+    }
+
+}
