5cd29105e3e5a567b7d9c67d01005914b8e2fc02
Andrey Globa <Globa.Andrey@gmail.com>
2020-06-30 14:43:05 +0300

    added changing after checking

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -11,24 +11,24 @@ public class CashbackHackServiceTest {
     @Test
     public void shouldTestMoreRemain() {
         // Сумма покупки менее 999$, требуется докупить
-        int expected = 1;
         int actual = test.remain(999);
+        int expected = 1;
         assertEquals(expected, actual);
     }
 
     @Test
     public void shouldTestRightRemain() {
         // Сумма покупки 1000$, докупать не требуется
-        int expected = test.remain(1000);
-        int actual = 0;
+        int expected = 0;
+        int actual = test.remain(1000);
         assertEquals(expected, actual);
     }
 
     @Test
     public void shouldTestMoreAndMoreRemain() {
         // Сумма покупки 1001$, требуется докупить
-        int expected = test.remain(1001);
-        int actual = 999;
+        int expected = 999;
+        int actual = test.remain(1001);
         assertEquals(expected, actual);
     }
 }
